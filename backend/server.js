67ec0a2b332540ca43d43a53ece8e9f25e37b5c8
Tekkila <gordogamer8@gmail.com>
2023-12-29 17:48:11 -0300

    Changes on cards

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const httpServer = createServer(app);
 const  cardValues= ['A', '2', '3', '4', '5', '6', '7', 'J', 'Q', 'K'];
 const cardSuits = ['Copas', 'Ouros', 'Paus', 'Espadas'];
 
+
+let players = [];
+
+
 function createCards() {
     let id = 1;
     const cards = [];
@@ -28,9 +32,21 @@ function shuffle(cards) {
         const j = Math.floor(Math.random() * (i + 1));
         [cards[i], cards[j]] = [cards[j], cards[i]];
     }
-    console.log(cards);
+
 }
 
+function distributeCards(cards, numberOfPlayers, cardsPerPlayer) {
+    const hand = [];
+
+    for (let player = 0; player < numberOfPlayers; player++) {
+        const playerHand = [];
+        for (let card = 0; card < cardsPerPlayer; card++) {
+            playerHand.push(cards.pop());
+        }
+        hand.push(playerHand);
+    }
+    return hand;
+}
 
 const io = new Server(httpServer, {
     cors: {
@@ -42,9 +58,19 @@ async function main() {
     io.on('connect', (socket) => {
         console.log('user connected', socket.id);
 
-        socket.on("start", () => {
+
+        socket.on("start", (playerName) => {
+            players.push({
+                id: socket.id,
+                name: playerName
+            });
+            
             const cards = createCards();
             shuffle(cards);
+            const hands = distributeCards(cards, 4, 3);
+            console.log(hands);
+            console.log(players);
+            io.emit('gameData', {hands, players});
         });
     })
 
--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <script setup>
   import { io } from "socket.io-client";
-  import { onMounted } from "vue";
+  import { onMounted, ref } from "vue";
 
   const socket = io("ws://localhost:3002");
+  const playerName = ref('');
 
   onMounted( () => {
     socket.on('connect', () => {
-
-    })
-    console.log("test")
+    });
     
+    socket.on('gameData', ({ hands, players }) => {
+      console.log("Mãos: ", hands);
+      console.log("Jogadores: ", players);
+    })
+    
+
+
   })
 
   function startGame(){
-      console.log('meu ovo');
-      socket.emit("start");
+      socket.emit("start", playerName.value);
     }
 
 </script>
 
 <template>
   <h1>hello</h1>
+  <input v-model="playerName" placeholder="Seu nome" />
   <button type="submit" @click="startGame">Start</button>
 </template>
