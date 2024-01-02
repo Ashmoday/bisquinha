b3e3ccc25553e91cb8bd5f4bf4d8ff4d8e67a2ef
Guilherme <guilhermeohnesorge1@gmail.com>
2024-01-02 13:32:12 -0300

    changesoncss

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <script setup>
   import { io } from "socket.io-client";
   import { onMounted, ref } from "vue";
-
   const socket = io("ws://localhost:3002");
   const playerName = ref('');
 
-  onMounted( () => {
+  onMounted(() => {
     socket.on('connect', () => {
     });
-    
+
     socket.on('gameData', ({ hands, players }) => {
       console.log("Mãos: ", hands);
       console.log("Jogadores: ", players);
-    })
-    
+    });
+  });
 
-
-  })
-
-  function startGame(){
-      socket.emit("start", playerName.value);
-    }
-
+  function startGame() {
+    socket.emit("start", playerName.value);
+  }
 </script>
-
 <template>
-  <h1>hello</h1>
-  <input v-model="playerName" placeholder="Seu nome" />
-  <button type="submit" @click="startGame">Start</button>
+  <div id="app">
+    <img src="./logo.png" alt="Logo" class="logo" />
+
+    <div class="center-container">
+      <div class="player-name-container">
+        <input v-model="playerName" placeholder="Seu nome" />
+        <button type="submit" @click="startGame">Jogar</button>
+      </div>
+    </div>
+  </div>
 </template>
