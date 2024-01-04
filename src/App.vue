3009aa1ef2da77bd5d4aeb7b2ad7b7f3ede34b08
Guilherme <guilhermeohnesorge1@gmail.com>
2024-01-04 15:00:04 -0300

    vascaoo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { io } from "socket.io-client";
   import { onMounted, ref } from "vue";
+
   const socket = io("ws://localhost:3002");
   let players = [];
   const playerName = ref('');
@@ -9,14 +10,11 @@
   const playedCards = ref([]);
   let currentPlayer = ref([]);
   let gameStarted = ref(false);
-
-
+  const enteringGame = ref(false); // Adicione esta linha
 
   onMounted(() => {
-    socket.on('connect', () => {
-    });
-       
-  })
+    socket.on('connect', () => {});
+  });
 
 
   socket.on('nextPlayer', (currentPlayerIndex) => {
@@ -39,18 +37,41 @@
       playerNames.value = playerNames;
     });
 
+    function applyWhiteBackground() {
+  const appElement = document.getElementById('app');
+  const logoElement = document.querySelector('.logo');
+
+  if (appElement && logoElement) {
+    appElement.style.backgroundColor = 'white';
+    appElement.style.border = '1px solid #636363';
+    appElement.style.width = '50%';
+    appElement.style.height = '100%'
+    appElement.style.margin = '0 auto';
+    appElement.style.background = 'linear-gradient(to right, #f0f0f0, white)';
+
+    logoElement.style.width = '150px'; 
+    logoElement.style.height = '150px'; 
+    logoElement.style.margin = '10px'; 
+    logoElement.style.position = 'fixed'; 
+    logoElement.style.top = '10px'; 
+    logoElement.style.left = '40px'; 
+  }
+
+  enteringGame.value = true;
+}
+
   function enterGame(){
       socket.emit("enterGame", playerName.value);
-      
+      applyWhiteBackground();
   }
 
   function startGame() {
-        socket.emit("start")
-        socket.on('gameData', ({ hands, playerNames }) => {
-        playerHands.value = hands;
-        playerNames.value = playerNames; 
-        gameStarted.value = true;
-      
+    socket.emit("start")
+    socket.on('gameData', ({ hands, playerNames }) => {
+      playerHands.value = hands;
+      playerNames.value = playerNames; 
+      gameStarted.value = true;
+      applyWhiteBackground(); 
     })
   }
   function playCard(card, cardOwner) {
@@ -115,45 +136,41 @@ function getCardSuitColor(cardSuit) {
 
 </script>
 <template>
-  <div>
+  <div :class="{ 'game-started': gameStarted, 'entering-game': enteringGame }">
     <img src="./logo.png" alt="Logo" class="logo" />
     <div class="center-container">
       <div class="player-name-container">
-      <input v-model="playerName" placeholder="Seu nome" />
-      <button type="submit" @click="enterGame">Entrar</button>
-    </div>
+        <input v-model="playerName" placeholder="Seu nome" />
+        <button type="submit" @click="enterGame">Entrar</button>
+      </div>
       <button id="iniciar_partida" type="submit" @click="startGame">Iniciar Partida</button>
-      </div>    
+    </div>    
 
-      <p v-if="gameStarted && currentPlayer">{{ currentPlayer.name }}</p>
+    <p v-if="gameStarted && currentPlayer">{{ currentPlayer.name }}</p>
 
-       <div v-for="(hand, index) in playerHands" :key="hand.id">
+    <div v-for="(hand, index) in playerHands" :key="hand.id">
       <p>{{ hand.name }}'s Mão do time {{ hand.team }}:</p>
       <ul class="card-container">
         <li v-for="card in hand.cards" :key="card.id" @click="playCard(card, hand.name)" class="card">
-          <span class="card-value" :style="{ color: getCardSuitColor(card.cardSuit) }"  >{{ card.cardValue }}</span>
+          <span class="card-value" :style="{ color: getCardSuitColor(card.cardSuit) }">{{ card.cardValue }}</span>
           <span class="card-suit" :style="{ color: getCardSuitColor(card.cardSuit) }" v-html="getCardSuitSymbol(card.cardSuit)"></span>
         </li>
       </ul>
     </div>
+
     <div v-if="playedCards.length > 0">
-    <h2>Cartas Jogadas:</h2>
-    <ul>
+      <h2>Cartas Jogadas:</h2>
+      <ul>
         <li v-for="(play, index) in playedCards" :key="index" class="played-card">
-            {{ play.card }} (Jogador: {{ play.cardOwner }})
+          {{ play.card }} (Jogador: {{ play.cardOwner }})
         </li>
-    </ul>
-</div>
+      </ul>
+    </div>
 
     <button type="submit" @click="buyCard">Buy Card</button>
-
     <button type="submit" @click="nextHand(playedCards)">Next Hand</button>
-
     <button type="submit" @click="selectTeam(1)">Time 1</button>
     <button type="submit" @click="selectTeam(2)">Time 2</button>
-
-    
-
   </div>
 </template>
 
