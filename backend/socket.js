e15df2ab5fef1cd414ded8d4d4b51fcb35ba3705
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-06-16 10:30:02 -0300

    Added playcard socket

--- backend/socket.js
+++ backend/socket.js
@@ -8,7 +8,10 @@ const {
     updateRooms,
     checkTeam,
     createGameCards,
-    nextPlayer
+    nextPlayer,
+    buyCardForPlayer,
+    playCard,
+    nextHand
 } = require('./gameManager');
 
 
@@ -19,7 +22,7 @@ const io = new Server(httpServer, {
     }
 });
 
-
+init(io)    
 
 async function main() {
 
@@ -57,60 +60,18 @@ async function main() {
         });
 
         socket.on("playCard", (roomId, card) => {
-            const game = games[roomId];
-            const player = game.players.find(player => player.id === socket.id);
-            if (game.playersWhoPlayedThisHand.includes(player.id)) return;
-            const currentPlayer = game.players[game.currentPlayerIndex];
-            if (player != currentPlayer) return;
-            const newPlayingHand = game.newPlayingHand;
-
-            const index = player.cards.findIndex(c => c.id === card.id);
-            const allCardsPlayed = game.allCardsPlayed;
-
-            if (card.cardValue === '7' && card.cardSuit === trump.cardSuit) {
-                const hasAceOfTrump = player.cards.some(c => c.cardValue === 'A' && c.cardSuit === trump.cardSuit);
-                let isLastRound = false;
-                if (player.cards.length < 2) {
-                    isLastRound = true;
-                }
-
-                if (!hasAceOfTrump && !isLastRound && newPlayingHand.length < 3 ) {
-                    console.log("Você só pode jogar o '7' de trunfo se tiver o ás de trunfo na mão ou for a última rodada.");
-                    return;
-                }   
-            }
-
-            if (card.cardValue === 'A' && card.cardSuit === trump.cardSuit) {
-                const hasSevenOfTrump = player.cards.some(c => c.cardValue === '7' && c.cardSuit === trump.cardSuit);
-                let isLastRound = false;
-                const sevenOutGame = (allCardsPlayed && allCardsPlayed.cards) ? allCardsPlayed.cards.some(c => c.cardValue === '7' && c.cardSuit === trump.cardSuit) : false;                    
-                
-                if (player.cards.length < 2) {
-                    isLastRound = true;
-                }
-                if (!hasSevenOfTrump && !isLastRound && !sevenOutGame) {
-                    console.log("Você só pode jogar Ás de trunfo se tiver o 7 na mão, for na última rodada OU o 7 já ter sido jogado!");
-                    return;
-                }
-
-                if (index !== -1)
-                {
-                    player.cards.splice(index, 1);
-                    game.newPlayingHand.push({
-                        card,
-                        cardOwner: player
-                    });
-                    allCardsPlayed.cards.push(card)
-                    game.playersWhoPlayedThisHand.push(socket.id)
-                    io.to(roomId).emit("gameData", game)
-                } else {
-                    console.log("Carta não encontrada na mão do jogador");
-                    return;
-                }
-
-                nextPlayer(game);
-            }
+            playCard(socket, roomId, card);
         })
+
+        socket.on("nextHand", (roomId) => {
+            let gameRoom = games[roomId];
+            if (!gameRoom) return;
+            nextHand(roomId)
+            
+        })
+
+
+
     })
 
 
